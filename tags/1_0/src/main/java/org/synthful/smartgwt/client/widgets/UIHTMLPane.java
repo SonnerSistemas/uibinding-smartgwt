4f5a641aa5abb1f2aaab0a11c160e24e1b49d5ba
lucasam <lucasam@81b8cabd-bf31-1424-a84e-eff5735e9861>
2010-09-15 19:41:46 +0000

    First version of uibinding-smartgwt

---
+++ tags/1_0/src/main/java/org/synthful/smartgwt/client/widgets/UIHTMLPane.java
@@ -0,0 +1,36 @@
+package org.synthful.smartgwt.client.widgets;
+
+import org.synthful.smartgwt.client.UIMasquerade;
+
+import com.google.gwt.user.client.ui.HasHTML;
+import com.google.gwt.user.client.ui.HasText;
+import com.smartgwt.client.widgets.HTMLPane;
+
+public class UIHTMLPane extends HTMLPane implements HasText, HasHTML, UIMasquerade<HTMLPane> {
+
+	@Override
+	public String getText() {
+		return super.getContents();
+	}
+
+	@Override
+	public void setText(String text) {
+		super.setContents(text);
+	}
+
+	@Override
+	public String getHTML() {
+		return super.getContents();
+	}
+
+	@Override
+	public void setHTML(String html) {
+		super.setContents(html);
+	}
+
+	@Override
+	public HTMLPane getSmartObject() {
+		return this;
+	}
+
+}
